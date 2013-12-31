357a504e9c86defcb3e79d7ff248666820fbf487
xuehua <txh_723@163.com>
2013-12-31 22:25:51 +0800

    add two files.

---
+++ src/EQueue/Broker/Client/ClientChannelInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EQueue.Remoting;
+
+namespace EQueue.Broker.Client
+{
+    public class ClientChannelInfo
+    {
+        public Channel Channel { get; private set; }
+        public string ClientId { get; private set; }
+        public int Version { get; private set; }
+        public DateTime LastUpdateTime { get; set; }
+
+        public ClientChannelInfo(Channel channel, string clientId, int version)
+        {
+            Channel = channel;
+            ClientId = clientId;
+            Version = version;
+            LastUpdateTime = DateTime.Now;
+        }
+    }
+}
---
+++ src/EQueue/Broker/Client/ConsumerGroupInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EQueue.Common;
+using EQueue.Remoting;
+
+namespace EQueue.Broker.Client
+{
+    public class ConsumerGroupInfo
+    {
+        private string _groupName;
+        private MessageModel _messageModel;
+        private ConcurrentDictionary<Channel, ClientChannelInfo> _clientChannelDict = new ConcurrentDictionary<Channel, ClientChannelInfo>();
+
+        public ConsumerGroupInfo(string groupName, MessageModel messageModel)
+        {
+            _groupName = groupName;
+            _messageModel = messageModel;
+        }
+
+        public IEnumerable<Channel> GetAllChannels()
+        {
+            return _clientChannelDict.Keys.ToArray();
+        }
+        public IEnumerable<string> GetAllClientIds()
+        {
+            return _clientChannelDict.Values.Select(x => x.ClientId).ToArray();
+        }
+    }
+}
