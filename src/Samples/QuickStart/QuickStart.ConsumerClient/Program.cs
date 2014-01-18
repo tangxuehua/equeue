d7dc668d4a017bf2beed2829ec988429f9021eeb
xuehua <txh_723@163.com>
2014-01-18 13:40:37 +0800

    modify consumer program.

--- src/Samples/QuickStart/QuickStart.ConsumerClient/Program.cs
+++ src/Samples/QuickStart/QuickStart.ConsumerClient/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using EQueue;
 using EQueue.Autofac;
 using EQueue.Clients.Consumers;
+using EQueue.Infrastructure.IoC;
+using EQueue.Infrastructure.Scheduling;
 using EQueue.JsonNet;
 using EQueue.Log4Net;
 using EQueue.Protocols;
@@ -29,6 +32,21 @@ namespace QuickStart.ConsumerClient
             var consumer3 = new Consumer("consumer3", ConsumerSettings.Default, "group1", MessageModel.Clustering, messageHandler3);
             messageHandler3.SetConsumer(consumer3);
             consumer3.Subscribe("SampleTopic").Start();
+
+            var messageHandler4 = new MessageHandler();
+            var consumer4 = new Consumer("consumer4", ConsumerSettings.Default, "group1", MessageModel.Clustering, messageHandler4);
+            messageHandler4.SetConsumer(consumer4);
+            consumer4.Subscribe("SampleTopic").Start();
+
+            var scheduleService = ObjectContainer.Resolve<IScheduleService>();
+            scheduleService.ScheduleTask(() =>
+            {
+                Console.WriteLine(string.Format("Queue Allocation. c1:{0}, c2:{1}, c3:{2}, c4:{3}",
+                    string.Join(",", consumer1.GetCurrentQueues().Select(x => x.QueueId)),
+                    string.Join(",", consumer2.GetCurrentQueues().Select(x => x.QueueId)),
+                    string.Join(",", consumer3.GetCurrentQueues().Select(x => x.QueueId)),
+                    string.Join(",", consumer4.GetCurrentQueues().Select(x => x.QueueId))));
+            }, 5000, 5000);
 
             Console.ReadLine();
         }
