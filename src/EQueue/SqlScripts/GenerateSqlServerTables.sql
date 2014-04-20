0446a9eacba07852bcc2873f1c6bffbd3f2b28d5
tangxuehua <txh_723@163.com>
2014-04-20 20:34:42 +0800

    add a new table generate sql script.

--- src/EQueue/SqlScripts/GenerateSqlServerTables.sql
+++ src/EQueue/SqlScripts/GenerateSqlServerTables.sql
@@ -11,3 +11,20 @@
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+CREATE TABLE [dbo].[queue_offset](
+    [version] [bigint] NOT NULL,
+    [consumer_group] [nvarchar](128) NOT NULL,
+    [topic] [nvarchar](128) NOT NULL,
+    [queue_id] [int] NOT NULL,
+    [queue_offset] [bigint] NOT NULL,
+    [timestamp] [datetime] NOT NULL,
+ CONSTRAINT [PK_queue_offset_1] PRIMARY KEY CLUSTERED 
+(
+    [consumer_group] ASC,
+    [topic] ASC,
+    [queue_id] ASC,
+    [version] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
