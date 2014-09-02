0bbe681b00717ad72c2b7d4324865ff17139599c
tangxuehua <txh_723@163.com>
2014-09-02 23:51:24 +0800

    提交遗漏的代码。

---
+++ src/EQueue/Broker/RequestHandlers/QueryTopicConsumeInfoRequestHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using ECommon.Components;
+using ECommon.Remoting;
+using ECommon.Serializing;
+using EQueue.Protocols;
+
+namespace EQueue.Broker.Processors
+{
+    public class QueryTopicConsumeInfoRequestHandler : IRequestHandler
+    {
+        private BrokerController _brokerController;
+        private IBinarySerializer _binarySerializer;
+        private IOffsetManager _offsetManager;
+
+        public QueryTopicConsumeInfoRequestHandler(BrokerController brokerController)
+        {
+            _brokerController = brokerController;
+            _binarySerializer = ObjectContainer.Resolve<IBinarySerializer>();
+            _offsetManager = ObjectContainer.Resolve<IOffsetManager>();
+        }
+
+        public RemotingResponse HandleRequest(IRequestHandlerContext context, RemotingRequest request)
+        {
+            var queryTopicConsumeInfoRequest = _binarySerializer.Deserialize<QueryTopicConsumeInfoRequest>(request.Body);
+            var topicConsumeInfoList = _offsetManager.QueryTopicConsumeInfos(queryTopicConsumeInfoRequest.GroupName, queryTopicConsumeInfoRequest.Topic).ToList();
+
+            topicConsumeInfoList.Sort((x, y) =>
+            {
+                var result = string.Compare(x.ConsumerGroup, y.ConsumerGroup);
+                if (result != 0)
+                {
+                    return result;
+                }
+                result = string.Compare(x.Topic, y.Topic);
+                if (result != 0)
+                {
+                    return result;
+                }
+                if (x.QueueId > y.QueueId)
+                {
+                    return 1;
+                }
+                else if (x.QueueId < y.QueueId)
+                {
+                    return -1;
+                }
+                return 0;
+            });
+
+            foreach (var topicConsumeInfo in topicConsumeInfoList)
+            {
+                var consumerGroup = _brokerController.ConsumerManager.GetConsumerGroup(topicConsumeInfo.ConsumerGroup);
+                topicConsumeInfo.HasConsumer = consumerGroup != null && consumerGroup.GetAllConsumerIds().Count() > 0;
+            }
+
+            var data = _binarySerializer.Serialize(topicConsumeInfoList);
+            return new RemotingResponse((int)ResponseCode.Success, request.Sequence, data);
+        }
+    }
+}
---
+++ src/EQueue/Protocols/TopicConsumeInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EQueue.Broker;
+
+namespace EQueue.Protocols
+{
+    [Serializable]
+    public class TopicConsumeInfo
+    {
+        /// <summary>消费者的分组
+        /// </summary>
+        public string ConsumerGroup { get; set; }
+        /// <summary>主题
+        /// </summary>
+        public string Topic { get; set; }
+        /// <summary>队列ID
+        /// </summary>
+        public int QueueId { get; set; }
+        /// <summary>消费位置
+        /// </summary>
+        public long ConsumedOffset { get; set; }
+        /// <summary>表示当前分组是否存在活跃（在线）的消费者
+        /// </summary>
+        public bool HasConsumer { get; set; }
+    }
+}
