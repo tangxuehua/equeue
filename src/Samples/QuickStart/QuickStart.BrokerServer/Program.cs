bc3829d64c368d5e4ac73a88d9f90c77cef0d026
tangxuehua <txh_723@163.com>
2015-05-23 22:39:22 +0800

    alert broker sample code.

--- src/Samples/QuickStart/QuickStart.BrokerServer/Program.cs
+++ src/Samples/QuickStart/QuickStart.BrokerServer/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Configuration;
+using System.Net;
 using ECommon.Autofac;
 using ECommon.Configurations;
 using ECommon.JsonNet;
 using ECommon.Log4Net;
+using ECommon.Utilities;
 using EQueue.Broker;
 using EQueue.Configurations;
+using ECommonConfiguration = ECommon.Configurations.Configuration;
 
 namespace QuickStart.BrokerServer
 {
@@ -13,13 +17,21 @@ namespace QuickStart.BrokerServer
         static void Main(string[] args)
         {
             InitializeEQueue();
-            BrokerController.Create().Start();
+
+            var bindingIP = ConfigurationManager.AppSettings["BindingAddress"];
+            var brokerEndPoint = string.IsNullOrEmpty(bindingIP) ? SocketUtils.GetLocalIPV4() : IPAddress.Parse(bindingIP);
+            var setting = new BrokerSetting {
+                ProducerIPEndPoint = new IPEndPoint(brokerEndPoint, 5000),
+                ConsumerIPEndPoint = new IPEndPoint(brokerEndPoint, 5001),
+                AdminIPEndPoint = new IPEndPoint(brokerEndPoint, 5002)
+            };
+            BrokerController.Create(setting).Start();
             Console.ReadLine();
         }
 
         static void InitializeEQueue()
         {
-            Configuration
+            ECommonConfiguration
                 .Create()
                 .UseAutofac()
                 .RegisterCommonComponents()
