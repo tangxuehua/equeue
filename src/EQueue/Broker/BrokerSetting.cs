41cb1bc1a939b1c6a761ee119360d907a519f72a
tangxuehua <txh_723@163.com>
2015-10-12 23:32:49 +0800

    修改默认值。

--- src/EQueue/Broker/BrokerSetting.cs
+++ src/EQueue/Broker/BrokerSetting.cs
@@ -57,7 +57,7 @@ namespace EQueue.Broker
         /// </summary>
         public TFChunkManagerConfig QueueChunkConfig { get; set; }
 
-        public BrokerSetting(string chunkFileStoreRootPath = @"c:\equeue-store", int messageChunkDataSize = 256 * 1024 * 1024, int chunkCacheMaxPercent = 90, int chunkCacheMinPercent = 60)
+        public BrokerSetting(string chunkFileStoreRootPath = @"c:\equeue-store", int messageChunkDataSize = 256 * 1024 * 1024, int chunkCacheMaxPercent = 75, int chunkCacheMinPercent = 40)
         {
             ProducerAddress = new IPEndPoint(SocketUtils.GetLocalIPV4(), 5000);
             ConsumerAddress = new IPEndPoint(SocketUtils.GetLocalIPV4(), 5001);
