0b789e9bcc947c30412e07cdccd00a7093e57ffb
tangxuehua <txh_723@163.com>
2016-05-01 10:05:40 +0800

    alter admin config.

--- src/EQueue.AdminWeb/Settings.cs
+++ src/EQueue.AdminWeb/Settings.cs
@@ -1,10 +1,24 @@
-﻿using System.Net;
+﻿using System.Configuration;
+using System.Net;
 using ECommon.Socketing;
 
 namespace EQueue.AdminWeb
 {
     public class Settings
     {
-        public static IPEndPoint BrokerAddress = new IPEndPoint(SocketUtils.GetLocalIPV4(), 5002);
+        public static IPEndPoint BrokerAddress;
+
+        static Settings() {
+            var brokerIp = ConfigurationManager.AppSettings["brokerIp"];
+
+            if (string.IsNullOrWhiteSpace(brokerIp))
+            {
+                BrokerAddress = new IPEndPoint(SocketUtils.GetLocalIPV4(), 5002);
+            }
+            else
+            {
+                BrokerAddress = new IPEndPoint(IPAddress.Parse(brokerIp), 5002);
+            }
+        }
     }
 }
